9410397995e472e77042c4fd0cb57361b403d601
DavidParrado <juandparrado04@gmail.com>
2024-04-06 21:36:10 -0500

    Fixing design for main screen

--- src/Gui/MainGUI.java
+++ src/Gui/MainGUI.java
@@ -17,11 +17,28 @@ public class MainGUI extends JFrame {
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setLocationRelativeTo(null);
 
+    // Create main title label
+    JLabel mainTitleLabel = new JLabel("Deluxe");
+    mainTitleLabel.setFont(new Font("Arial", Font.BOLD, 36));
+    mainTitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+    // Create welcome label
+    JLabel welcomeLabel = new JLabel("Bienvenido selecciona el modulo al que quieres acceder");
+    welcomeLabel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+    welcomeLabel.setFont(new Font("Arial", Font.BOLD, 22));
+    welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
     // Create buttons
     UsuarioButton = new JButton("Usuario");
     CategoriaButton = new JButton("Categoria");
     ProductoButton = new JButton("Producto");
     MarcaButton = new JButton("Marca");
+
+    // Adding fonts buttons
+    UsuarioButton.setFont(new Font("Montserrat", Font.BOLD, 18));
+    CategoriaButton.setFont(new Font("Montserrat", Font.BOLD, 18));
+    ProductoButton.setFont(new Font("Montserrat", Font.BOLD, 18));
+    MarcaButton.setFont(new Font("Montserrat", Font.BOLD, 18));
 
     // Add action listeners to buttons
     UsuarioButton.addActionListener(new ActionListener() {
@@ -56,16 +73,28 @@ public class MainGUI extends JFrame {
       }
     });
 
-    // Create a panel to hold the buttons
-    JPanel buttonPanel = new JPanel(new GridLayout(4, 1));
+    // Create a panel to hold the main title label, welcome label, and buttons
+    JPanel mainPanel = new JPanel(new BorderLayout());
+    mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 50, 50, 50)); // Add padding
+
+    JPanel titlePanel = new JPanel(new BorderLayout());
+    titlePanel.add(mainTitleLabel, BorderLayout.NORTH);
+
+    titlePanel.add(welcomeLabel, BorderLayout.CENTER);
+    mainPanel.add(titlePanel, BorderLayout.NORTH);
+
+    JPanel buttonPanel = new JPanel(new GridLayout(4, 1, 10, 10)); // Add vertical spacing between buttons
+    buttonPanel.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0)); // Add top padding
     buttonPanel.add(UsuarioButton);
     buttonPanel.add(CategoriaButton);
     buttonPanel.add(ProductoButton);
     buttonPanel.add(MarcaButton);
+    mainPanel.add(buttonPanel, BorderLayout.CENTER);
 
     // Add the panel to the frame
-    getContentPane().add(buttonPanel, BorderLayout.CENTER);
+    getContentPane().add(mainPanel, BorderLayout.CENTER);
   }
+
   public static void main(String[] args) {
     // Create and show the GUI
     SwingUtilities.invokeLater(new Runnable() {
